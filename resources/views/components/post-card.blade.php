7ee5a7b48c1244eeaccf2ee4a3ea853d504477ab
Jacob Jeffries <jjeffrie@nmu.edu>
2023-10-16 16:32:09 -0400

    Slightly edited the Read More button on the regular post cards.

--- resources/views/components/post-card.blade.php
+++ resources/views/components/post-card.blade.php
@@ -26,15 +26,15 @@
                 {!! $post->excerpt !!}
             </div>
 
-            <footer class="flex justify-between items-center mt-8">
-                <div class="flex items-center text-sm">
+            <footer class="items-center mt-8">
+                <div class="flex items-center text-xs">
                     <img src="/images/lary-avatar.svg" alt="Lary avatar">
                     <div class="ml-3">
                         <h5 class="font-bold">{{ $post->user->name }}</h5>
                     </div>
                 </div>
 
-                <div class="hidden lg:block">
+                <div class="text-sm mt-8 float-right">
                     <a href="/post/{{ $post->id }}"
                         class="transition-colors duration-300 text-xs font-semibold bg-gray-200 hover:bg-gray-300 rounded-full py-2 px-8"
                     >Read More</a>
